b6fcc94b80a6d84b5bbaa975516c526183219923
Frank Lyder bredland <fl@nyggen.com>
2015-02-05 18:13:38 +0100

    scss compiler setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp'),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    sass = require('gulp-sass');
 
 var config = {
-    bowerDir: './src/main/webapp/vendor/'
+    bowerDir: './src/main/webapp/vendor/',
+    sassFiles:  './src/main/webapp/sass/*.scss',
+    cssOutputDir: './src/main/webapp/css/'
 };
 
-gulp.task('default', ['bower']);
+gulp.task('default', ['bower', 'sass']);
 
 gulp.task('bower', function() {
     return bower()
         .pipe(gulp.dest(config.bowerDir));
 });
 
+gulp.task('sass', function () {
+    return gulp.src(config.sassFiles)
+        .pipe(sass())
+        .pipe(gulp.dest(config.cssOutputDir));
+});
+
+gulp.task('watch', ['default'], function () {
+    gulp.watch('./bower.json', ['bower']);
+    gulp.watch(config.sassFiles, ['sass']);
+})
