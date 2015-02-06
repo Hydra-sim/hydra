68e196ca80787b2f3e1b497574a308284cdcef96
SimenHjelden <hjesim12@student.westerdals.no>
2015-02-06 08:41:30 +0100

    La til felt for passenger for buss og la inn variablene likt slik det andre som det lå i controller. Skifta også til stor tittel tekst i index filen.

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -9,14 +9,16 @@
 
     app.controller('ApplicationController', ['$scope', 'Simulation', function ($scope, Simulation) {
         $scope.timeBetweenBuses = 10;
+        $scope.passengersPerBus = 0;
         $scope.numberOfEntrances = 1;
         $scope.days = 0;
         $scope.hours = 1;
         $scope.minutes = 0;
 
-        $scope.runSim = function(timeBetweenBuses, numberOfEntrances, days, hours, minutes) {
+        $scope.runSim = function(timeBetweenBuses,passengersPerBus, numberOfEntrances, days, hours, minutes) {
             var sim = new Simulation({
                 'timeBetweenBuses': timeBetweenBuses,
+                'passengersPerBus': passengersPerBus,
                 'numberOfEntrances': numberOfEntrances,
                 'days': days,
                 'hours': hours,
