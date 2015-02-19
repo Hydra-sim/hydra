1113b7e1f1c3ae364127d0b68c7443556aee6325
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2015-02-19 09:43:47 +0100

    Commented calculations.Simulation

--- src/main/java/calculations/Simulation.java
+++ src/main/java/calculations/Simulation.java
@@ -10,14 +10,18 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by kristinesundtlorentzen on 4/2/15.
+ * This class represents the simulation engine in its current state.
+ *
+ * @author Kristine Sundt Lorentzen
  */
 public class Simulation {
 
+    //region attributes
     private List<Consumer> consumers;
     private List<Producer> producers;
     private int ticks;
     List<Entity> entities;
+    //endregion
 
     //region constructors
     public Simulation() {
@@ -67,6 +71,18 @@ public class Simulation {
     //endregion
 
     //region simulation
+
+    /**
+     * This method simulates the trafic flow from the producers, through all the cosumers.
+     *
+     * Changelog:
+     *
+     * 13.02.2015, Kristine: The algorithm uses one list of producers and one list of consumers to add entities to
+     * and remove entities from the list over entities. It uses the list of entities to check which one has the
+     * highest waiting time at the end of the simulation.
+     *
+     * @return A {@link pojos.SimulationData} object with entities consumed, entities left in queue and max waiting time.
+     */
     public SimulationData simulate() {
 
         entities = new ArrayList<>();
@@ -90,6 +106,16 @@ public class Simulation {
     }
 
     //region simulation methods
+
+    /**
+     * Checks which entity has the longest waiting time registered on it, and checks if this is higher than the highest
+     * waiting time registered so far in the simulation.
+     *
+     * @param maxWaitingTime The largest of the registered waiting times so far in the simulation
+     *
+     * @return Whichever value is largest of the registered waiting times so far in the simulation and the highest
+     *         waiting time of the entities registered on the entities list.
+     */
     private int calculateWaitingTime(int maxWaitingTime) {
 
         if(entities.size() == 0) return 0;
@@ -101,6 +127,14 @@ public class Simulation {
 
     }
 
+    /**
+     * Takes the list of entities and deletes them from the list of entities according to the number and strength of
+     * the consumers registered in the simulation. Every time an entity is deleted, the method adds 1 to the number of
+     * entities consumed.
+     * @param entitiesConsumed The number of entites consumed so far in the simulation
+     * @return The number of entities consumed so far in the simulation + the number of entities consumed during the
+     *         running of the method.
+     */
     private int consumeEntities(int entitiesConsumed) {
 
         for(Consumer consumer : consumers) {
@@ -115,11 +149,18 @@ public class Simulation {
         return entitiesConsumed;
     }
 
-    private void addEntities(int i) {
+    /**
+     * Adds entities to the list of entities according to the number and strength of the producers registered in the
+     * simulation.
+     *
+     * @param currentTick The current tick number the simulation is on, to check if it is time for the producer to
+     *                    produce entities.
+     */
+    private void addEntities(int currentTick) {
 
         for(Producer producer : producers) {
 
-            if(i == 0 || producer.getTicksToWait() == 0 || i % producer.getTicksToWait() == 0) {
+            if(currentTick == 0 || producer.getTicksToWait() == 0 || currentTick % producer.getTicksToWait() == 0) {
 
                 for(int j = 0; j < producer.getEntitiesToProduce(); j++) {
                     entities.add(new Entity(1));
@@ -128,6 +169,9 @@ public class Simulation {
         }
     }
 
+    /**
+     * Increases the waiting time of all the entities that have not been consumed by 1.
+     */
     private void increaseWaitingTime() {
 
         for(Entity entity : entities) {
