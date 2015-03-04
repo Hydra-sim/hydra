ab92569a7392db7f201e112fe90d1f0779992fb7
Frank Lyder bredland <fl@nyggen.com>
2015-03-04 21:29:14 +0100

    More graph stuff

--- src/main/webapp/js/graph.js
+++ src/main/webapp/js/graph.js
@@ -67,9 +67,6 @@
             .on("drag", function(args){
                 thisGraph.state.justDragged = true;
                 thisGraph.dragmove.call(thisGraph, args);
-            })
-            .on("dragend", function() {
-                // todo check if edge-mode is selected
             });
 
         // listen for key events
@@ -105,62 +102,6 @@
             });
 
         svg.call(dragSvg).on("dblclick.zoom", null);
-
-        // listen for resize
-        window.onresize = function(){thisGraph.updateWindow(svg);};
-
-        // handle download data
-        d3.select("#download-input").on("click", function(){
-            var saveEdges = [];
-            thisGraph.edges.forEach(function(val, i){
-                saveEdges.push({source: val.source.id, target: val.target.id});
-            });
-            var blob = new Blob([window.JSON.stringify({"nodes": thisGraph.nodes, "edges": saveEdges})], {type: "text/plain;charset=utf-8"});
-            saveAs(blob, "mydag.json");
-        });
-
-
-        // handle uploaded data
-        d3.select("#upload-input").on("click", function(){
-            document.getElementById("hidden-file-upload").click();
-        });
-        d3.select("#hidden-file-upload").on("change", function(){
-            if (window.File && window.FileReader && window.FileList && window.Blob) {
-                var uploadFile = this.files[0];
-                var filereader = new window.FileReader();
-
-                filereader.onload = function(){
-                    var txtRes = filereader.result;
-                    // TODO better error handling
-                    try{
-                        var jsonObj = JSON.parse(txtRes);
-                        thisGraph.deleteGraph(true);
-                        thisGraph.nodes = jsonObj.nodes;
-                        thisGraph.setIdCt(jsonObj.nodes.length + 1);
-                        var newEdges = jsonObj.edges;
-                        newEdges.forEach(function(e, i){
-                            newEdges[i] = {source: thisGraph.nodes.filter(function(n){return n.id == e.source;})[0],
-                                target: thisGraph.nodes.filter(function(n){return n.id == e.target;})[0]};
-                        });
-                        thisGraph.edges = newEdges;
-                        thisGraph.updateGraph();
-                    }catch(err){
-                        window.alert("Error parsing uploaded file\nerror message: " + err.message);
-                        return;
-                    }
-                };
-                filereader.readAsText(uploadFile);
-
-            } else {
-                alert("Your browser won't let you save this graph -- try upgrading your browser to IE 10+ or Chrome or Firefox.");
-            }
-
-        });
-
-        // handle delete graph
-        d3.select("#delete-graph").on("click", function(){
-            thisGraph.deleteGraph(false);
-        });
     };
 
     GraphCreator.prototype.setIdCt = function(idct){
@@ -192,19 +133,6 @@
         }
     };
 
-    GraphCreator.prototype.deleteGraph = function(skipPrompt){
-        var thisGraph = this,
-            doDelete = true;
-        if (!skipPrompt){
-            doDelete = window.confirm("Press OK to delete this graph");
-        }
-        if(doDelete){
-            thisGraph.nodes = [];
-            thisGraph.edges = [];
-            thisGraph.updateGraph();
-        }
-    };
-
     /* select all text in element: taken from http://stackoverflow.com/questions/6139107/programatically-select-text-in-a-contenteditable-html-element */
     GraphCreator.prototype.selectElementContents = function(el) {
         var range = document.createRange();
@@ -229,7 +157,6 @@
                 tspan.attr('x', 0).attr('dy', '15');
         }
     };
-
 
     // remove edges associated with a node
     GraphCreator.prototype.spliceLinksForNode = function(node) {
@@ -556,14 +483,6 @@
         this.state.justScaleTransGraph = true;
         d3.select("." + this.consts.graphClass)
             .attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
-    };
-
-    GraphCreator.prototype.updateWindow = function(svg){
-        var docEl = document.documentElement,
-            bodyEl = document.getElementsByTagName('body')[0];
-        var x = window.innerWidth || docEl.clientWidth || bodyEl.clientWidth;
-        var y = window.innerHeight|| docEl.clientHeight|| bodyEl.clientHeight;
-        svg.attr("width", x).attr("height", y);
     };
 
     angular
