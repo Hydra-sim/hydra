a1f0a6b75dcf1c8e86b51e3aad3dacc6ec8dcad3
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2015-03-09 12:23:55 +0100

    Started implementing creation of consumers and producers. (not functional yet)

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -145,4 +145,60 @@
         };
     });
 
+    app.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+        $scope.openProducerModal = function (size) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'producerModal.html',
+                controller: 'ModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.openConsumerModal = function (size) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'consumerModal.html',
+                controller: 'ConsumerModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            $scope.entitesConsumedPerTick = 10;
+        }
+    });
+
+    app.controller('ConsumerModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+        $scope.items = items;
+
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
+
 })();
