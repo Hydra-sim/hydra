e7a90eb4cc76eff1e64c10e9230bd2d6c0b53175
Frank Lyder bredland <fl@nyggen.com>
2015-03-26 00:50:53 +0200

    Some refactoring

--- src/main/webapp/js/graph.js
+++ src/main/webapp/js/graph.js
@@ -34,9 +34,50 @@
                         DELETE_KEY: 8
                     };
 
-                    // Circle to move, and selected circle
+                    // Selected circle
                     var selectedCircle = null;
-                    var circleToMoveData = null;
+
+                    function selectCircle(circleToSelect) {
+                        // Unselect old circle
+                        unselectCircle();
+
+                        // Set the selected circle
+                        selectedCircle = circleToSelect;
+                        selectedCircle.classed(consts.selectedClass, true);
+                    }
+
+                    function unselectCircle() {
+                        // Remove class from old selection if any
+                        if(circleIsSelected())
+                            selectedCircle.classed(consts.selectedClass, false);
+
+                        selectedCircle = null;
+                    }
+
+                    function deleteSelectedCircle() {
+                        // Keep a copy of the circle to delete before unselecting it
+                        var circleToDelete = selectedCircle;
+                        unselectCircle();
+
+                        // TODO: delete selected node
+                    }
+
+                    function circleIsSelected() {
+                        return selectedCircle != undefined && selectedCircle != null;
+                    }
+
+                    // Circle to move
+                    var circleToMove = null;
+
+                    function setCircleToMove(circle) {  circleToMove = circle; }
+                    function resetCricleToMove() {  circleToMove = null; }
+                    function circleToMoveNotEmpty() { return circleToMove != null; }
+
+                    // data = [0: x, 1: y]
+                    function setCircleToMovePosition(data) {
+                        circleToMove.x = data[0];
+                        circleToMove.y = data[1];
+                    }
 
                     // Create the svg element
                     var svg = d3.select(element[0])
@@ -56,11 +97,8 @@
                     // If something is selected and you move the mouse
                     svg.on("mousemove", function() {
                         // If a circle is selected lets move it
-                        if(circleToMoveData != null) {
-                            var mouse = d3.mouse(this);
-                            console.log(circleToMoveData);
-                            circleToMoveData.x = mouse[0];
-                            circleToMoveData.y = mouse[1];
+                        if(circleToMoveNotEmpty()) {
+                            setCircleToMovePosition(d3.mouse(this));
                             update();
                         }
                     });
@@ -69,28 +107,21 @@
 
                     // If someone tries to delete something
                     d3.select("body").on("keydown", function(d) {
-                        if(selectedCircle != undefined && selectedCircle != null) {
-                            if (d3.event.keyCode == consts.DELETE_KEY) {
-                                d3.event.preventDefault();
+                        if (d3.event.keyCode == consts.DELETE_KEY) {
+                            d3.event.preventDefault();
 
-                                // TODO: delete nodeg
-                            }
+                            if(circleIsSelected())
+                                deleteSelectedCircle();
                         }
                     });
 
-                    function unselectCircle() {
-                        // Remove class from old selection if any
-                        if(selectedCircle != undefined && selectedCircle != null)
-                            selectedCircle.classed(consts.selectedClass, false);
-
-                        selectedCircle = null;
-                    }
-
                     // Update function, updating nodes and edges
                     function update() {
+                        function transformFunction(d){return "translate(" + d.x + "," + d.y + ")";}
+
                         // update existing nodes
-                        circles = circles.data(scope.nodes, function(d){ return d.id;});
-                        circles.attr("transform", function(d){return "translate(" + d.x + "," + d.y + ")";});
+                        circles = circles.data(scope.nodes, function(d){ return d.id; });
+                        circles.attr("transform", transformFunction);
 
                         // add new nodes
                         var newGs= circles
@@ -99,19 +130,12 @@
 
                         newGs
                             .attr('class', function(d) { return d.type + " " + consts.circleGClass; })
-                            .attr("transform", function(d){return "translate(" + d.x + "," + d.y + ")";})
-                            .on("mousedown", function(d){
-                                circleToMoveData = d;
-                            })
+                            .attr("transform", transformFunction)
+                            .on("mousedown", setCircleToMove)
                             .on("mouseup", function(){
                                 d3.event.stopPropagation();
-                                circleToMoveData = null;
-
-                                unselectCircle();
-
-                                // Set the selected circle
-                                selectedCircle = d3.select(this);
-                                selectedCircle.classed(consts.selectedClass, true);
+                                resetCricleToMove();
+                                selectCircle(d3.select(this));
                             });
 
                         newGs
