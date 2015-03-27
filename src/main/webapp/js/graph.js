4173db3ddaf45290899f18095361dd90c5b437be
Frank Lyder bredland <fl@nyggen.com>
2015-03-27 13:02:28 +0200

    Moved end-arrow marker to the correct place

--- src/main/webapp/js/graph.js
+++ src/main/webapp/js/graph.js
@@ -132,7 +132,7 @@
                     defs.append('svg:marker')
                         .attr('id', 'end-arrow')
                         .attr('viewBox', '0 -5 10 10')
-                        .attr('refX', "32")
+                        .attr('refX', "17")
                         .attr('markerWidth', 3.5)
                         .attr('markerHeight', 3.5)
                         .attr('orient', 'auto')
