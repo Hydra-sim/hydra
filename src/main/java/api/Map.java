c9e61d71ff611fd10d85eeeed8ceb62b76c666f4
Frank Lyder bredland <fl@nyggen.com>
2015-04-09 12:47:40 +0200

    tried making a fileupload

--- src/main/java/api/Map.java
+++ src/main/java/api/Map.java
@@ -1,13 +1,10 @@
 package api;
 
-import com.sun.jersey.core.header.FormDataContentDisposition;
-import com.sun.jersey.multipart.FormDataParam;
-import org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,7 +22,6 @@ public class Map {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-
     public Response list()
     {
         TypedQuery<models.Map> query = entityManager.createNamedQuery(
@@ -33,7 +29,7 @@ public class Map {
                 models.Map.class
         );
 
-        return Response.ok( query.getResultList() ).build();
+        return Response.ok(query.getResultList()).build();
     }
 
 
@@ -55,87 +51,36 @@ public class Map {
         return Response.ok( item.getUrl() ).build();
     }
 
-    @Transactional
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("{id}")
-    public Response delete(@PathParam("id") int id)
-    {
-        try {
-            models.Map item = entityManager.find(models.Map.class, id);
-            entityManager.remove(item);
-        }
-        catch (Exception e) {
-            return Response.serverError().build();
-        }
-
-        return Response.ok().build();
-    }
-
-    @Transactional
     @POST
-    @Path("upload")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response upload(@DefaultValue("true") @FormDataParam("enabled") boolean enabled,
-                           @FormDataParam("file") InputStream uploadedInputStream,
-                           @FormDataParam("file") FormDataContentDisposition fileDetail) {
+    @Path("/upload-file")
+    @Consumes("multipart/form-data")
+    public Response uploadFile(@MultipartForm FileUploadForm form) {
 
-        //Your local disk path where you want to store the file
-        String uploadedFileLocation = fileDetail.getFileName();
-        System.out.println(uploadedFileLocation);
-        // save it
-        File  objFile=new File(uploadedFileLocation);
-        if(objFile.exists())
+        String fileName = form.getFileName() == null ? "Unknown" : form.getFileName() ;
+
+        String completeFilePath = "c:/temp/" + fileName;
+        try
         {
-            objFile.delete();
+            //Save the file
+            File file = new File(completeFilePath);
 
-        }
-
-        saveToFile(uploadedInputStream, uploadedFileLocation);
-
-        models.Map map = new models.Map(uploadedFileLocation);
-        entityManager.persist(map);
-        String output = "File uploaded via Jersey based RESTFul Webservice to: " + uploadedFileLocation;
-        return Response.status(200).entity(output).build();
-    }
-
-    private void saveToFile(InputStream uploadedInputStream,
-                            String uploadedFileLocation) {
-
-        try {
-            OutputStream out = null;
-            int read = 0;
-            byte[] bytes = new byte[1024];
-
-            out = new FileOutputStream(new File(uploadedFileLocation));
-            while ((read = uploadedInputStream.read(bytes)) != -1) {
-                out.write(bytes, 0, read);
+            if (!file.exists())
+            {
+                file.createNewFile();
             }
-            out.flush();
-            out.close();
-        } catch (IOException e) {
 
+            FileOutputStream fos = new FileOutputStream(file);
+
+            fos.write(form.getFileData());
+            fos.flush();
+            fos.close();
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
-
-    }
-
-
-    @Transactional
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("{id}")
-    public Response edit(@PathParam("id") int id, models.Timetable timetable)
-    {
-        try {
-            entityManager.merge(timetable);
-        }
-        catch (Exception e) {
-            return Response.serverError().build();
-        }
-
-        return Response.ok().build();
+        //Build a response to return
+        return Response.status(200)
+            .entity("uploadFile is called, Uploaded file name : " + fileName).build();
     }
 }
