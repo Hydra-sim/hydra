7029ccace4759da2bee916f6169028dd7e9dfe2e
Frank Lyder bredland <fl@nyggen.com>
2015-04-26 00:21:00 +0200

    Simplefied SimulationFactory some

--- src/main/java/factory/SimulationFactory.java
+++ src/main/java/factory/SimulationFactory.java
@@ -21,56 +21,41 @@ public class SimulationFactory {
     private dao.Timetable timetableDao;
 
     public Simulation createSimulation(SimulationFormData input) throws Exception {
-        List<Producer> producers = new ArrayList<>();
-        List<Consumer> consumers = new ArrayList<>();
-        List<ConsumerGroup> consumerGroups = new ArrayList<>();
-
+        List<Node> nodes = new ArrayList<>();
         List<Relationship> relationships = new ArrayList<>();
 
         for (SimulationNode node : input.nodes) {
             if (isProducer(node)) {
                 Timetable timetable = timetableDao.get(node.timetableId);
-                producers.add(createProducer(node, timetable));
+                nodes.add(createProducer(node, timetable));
 
             } else if (isConsumer(node)) {
-                consumers.add(createConsumer(node));
+                nodes.add(createConsumer(node));
 
             } else if (isConsumerGroup(node)) {
                 ConsumerGroup consumerGroup = new ConsumerGroup(node.consumerGroupName, node.numberOfConsumers, node.ticksToConsumeEntity);
-                consumerGroups.add(consumerGroup);
+                nodes.add(consumerGroup);
             }
         }
 
         for (SimulationEdge edge : input.edges) {
-            Node source = findNodeWithId(edge.source.id, (List<Node>)(List<?>)producers, (List<Node>)(List<?>)consumers);
-            Node target = findNodeWithId(edge.target.id, (List<Node>)(List<?>)producers, (List<Node>)(List<?>)consumers);
+            Node source = findNodeWithId(edge.source.id, nodes).get();
+            Node target = findNodeWithId(edge.target.id, nodes).get();
 
-            Relationship relationship = new Relationship(source,target,edge.weight);
+            Relationship relationship = new Relationship(source, target, edge.weight);
             relationships.add(relationship);
         }
 
-        List<Node> nodes = new ArrayList<>();
-
-        nodes.addAll(producers);
-        nodes.addAll(consumers);
-        nodes.addAll(consumerGroups);
-
         // Create the simulation
-        return new models.Simulation(input.name, new Date(), nodes, relationships, input.startTick, input.ticks, input.breakpoints);
-    }
-
-    private Node findNodeWithId(int id, List<Node> list1, List<Node> list2) throws RuntimeException {
-        Optional<Node> node1 = findNodeWithId(id, list1);
-
-        if(node1.isPresent())
-            return node1.get();
-
-        Optional<Node> node2 = findNodeWithId(id, list2);
-
-        if(node2.isPresent())
-            return node2.get();
-
-        throw new RuntimeException("No node with id found");
+        return new models.Simulation(
+                input.name,
+                new Date(),
+                nodes,
+                relationships,
+                input.startTick,
+                input.ticks,
+                input.breakpoints
+        );
     }
 
     private Optional<Node> findNodeWithId(int id, List<Node> nodes) {
