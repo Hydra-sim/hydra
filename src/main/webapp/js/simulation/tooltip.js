93065feda4c5aff303a65d870527863cf841ee67
SimenHjelden <hjesim12@student.westerdals.no>
2015-04-26 12:44:55 +0200

    Some changes i made last night to try to fix tooltip directory

--- src/main/webapp/js/simulation/tooltip.js
+++ src/main/webapp/js/simulation/tooltip.js
@@ -1,36 +1,40 @@
 (function() {
 
+    'use strict';
+
     var app = angular.module('simulation', []);
 
-    app.controller('TooltipTestCtrl', function($scope) {
+    app.controller('TooltipTestCtrl', function($scope){
+        var tooltip = angular.element(document.getElementsByClassName('custom-tooltip'));
+        var path = "../templates/tooltip/";
 
-    });
+        $scope.open = function(templateUrl, posX, posY){
+            tooltip.empty();
+            tooltip.append(
+                $compile("<div class='inner-tooltip' ng-include='\"" + path + templateUrl + "\"'></div>")
+                ($scope));
+            tooltip.css({
+                "position": "absolute",
+                "display": "block",
+                "opacity": "1",
+                "left": posX + "px",
+                "top": posY + "px"
+            });
+        }
+    })
 
-    app.directive("myTooltipTemplate", function ($compile) {
+    app.directive("customTooltip", function($compile) {
         return {
             template: "<div class='custom-tooltip'></div>",
-            restrict: "E",
+            restrict: "A",
+            replace: true,
             transclude: true,
-            scope: {
-                control: '='
-            },
             link: function (scope, element, attrs) {
 
-                var tooltipElement = element[0];
-                var path = "templates/tooltip/"
+                var graph = document.querySelector('graph');
 
-                function open (templateUrl, posX, posY){
-                    tooltipElement.empty();
-                    tooltipElement.append("<div ng-include='\"" + path + templateUrl + "\"'></div>")
-                    tooltipElement.css({
-                        position: "absolute",
-                        display: "block",
-                        left: posX + "px",
-                        top: posY + "px"
-                    });
-                }
             }
-        };
+        }
 
     });
 })()
