97972f319afc4c6e31aecaad9f182dac65883463
Frank Lyder bredland <fl@nyggen.com>
2015-04-26 17:59:12 +0200

    Moved dublicate controller

--- src/main/webapp/js/simulation/tooltip.js
+++ src/main/webapp/js/simulation/tooltip.js
@@ -23,6 +23,29 @@
         }
     });
 
+    /*
+     * Moved dublicate controller from controller.js in here
+     * /
+    app.controller('TooltipTestCtrl', function($scope){
+        var tooltip = angular.element(document.getElementsByClassName('custom-tooltip'));
+        var path = "../templates/tooltip/";
+
+        $scope.open = function(templateUrl, posX, posY){
+            tooltip.empty();
+            tooltip.append(
+                $compile("<div class='inner-tooltip' ng-include='\"" + path + templateUrl + "\"'></div>")
+                ($scope));
+            tooltip.css({
+                "position": "absolute",
+                "display": "block",
+                "opacity": "1",
+                "left": posX + "px",
+                "top": posY + "px"
+            });
+        }
+    });
+    /**/
+
     app.directive("customTooltip", function($compile) {
         return {
             template: "<div class='custom-tooltip'></div>",
