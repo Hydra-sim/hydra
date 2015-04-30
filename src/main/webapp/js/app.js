43af38d6e05ea607753919f98b60f0ede04661ba
Frank Lyder bredland <fl@nyggen.com>
2015-04-30 18:41:37 +0200

    Added fulldocumentation

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -49,7 +49,7 @@
 
             .when('/map',               {controller: 'UploadMapCtrl',           templateUrl: "templates/map.html"})
             .when('/tooltip',           {controller: 'TooltipTestCtrl',         templateUrl: "templates/tooltip/testTooltip.html"})
-            .when('/documentation',     {controller: 'FullDocumentationCtrl',   templateUrl: 'templates/fullDocumentation.html' })
+            .when('/documentation',     {controller: 'FullDocumentationCtrl',   templateUrl: 'templates/fulldocumentation.html' })
             .when('/loadingscreen',     {templateUrl: 'templates/simulation/loadingscreen.html'})
 
             .otherwise({redirectTo : '/'})
