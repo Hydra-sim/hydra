293ce59ba460a40c5a6a110134a414acdf8d88f5
SimenHjelden <hjesim12@student.westerdals.no>
2015-05-01 12:08:56 +0200

    Fixed the arrow width and end-marker between nodes

--- src/main/webapp/js/graph/grapheditor.js
+++ src/main/webapp/js/graph/grapheditor.js
@@ -25,8 +25,8 @@ var GraphCreator = function(svg, nodes, edges){
         .attr('id', 'end-arrow')
         .attr('viewBox', '0 -5 10 10')
         .attr('refX', "32")
-        .attr('markerWidth', 7)
-        .attr('markerHeight', 7)
+        .attr('markerWidth', 3.5)
+        .attr('markerHeight', 3.5)
         .attr('orient', 'auto')
         .append('svg:path')
         .attr('d', 'M0,-5L10,0L0,5');
@@ -36,8 +36,8 @@ var GraphCreator = function(svg, nodes, edges){
         .attr('id', 'mark-end-arrow')
         .attr('viewBox', '0 -5 10 10')
         .attr('refX', 7)
-        .attr('markerWidth', 7)
-        .attr('markerHeight', 7)
+        .attr('markerWidth', 3.5)
+        .attr('markerHeight', 3.5)
         .attr('orient', 'auto')
         .append('svg:path')
         .attr('d', 'M0,-5L10,0L0,5');
