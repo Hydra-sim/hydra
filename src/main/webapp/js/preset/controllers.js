2451d5bdd6b0c2405f85a42479d30dbc56105fd8
SimenHjelden <hjesim12@student.westerdals.no>
2015-05-01 13:34:27 +0200

    Fixed confermation on location/preset aswell

--- src/main/webapp/js/preset/controllers.js
+++ src/main/webapp/js/preset/controllers.js
@@ -11,14 +11,31 @@
         $rootScope.menu_field_button_click = function () {};
     });
 
-    app.controller('PresetListCtrl', function($scope, $rootScope, Preset) {
+    app.controller('PresetListCtrl', function($scope, $rootScope, $modal, Preset) {
         function updatePresetScope() {
             $scope.presets = Preset.query({});
         }
         updatePresetScope();
 
         $scope.deletePreset = function(id) {
-            Preset.delete({}, {"id": id}, updatePresetScope);
+            var modalInstance = $scope.confirmation();
+
+            modalInstance.result.then(function (selectedItem) {
+                Preset.delete({}, {"id": id}, updatePresetScope);
+            });
+        };
+
+        $scope.confirmation = function() {
+
+            $scope.confirmed = false;
+
+            var modalInstance = $modal.open({
+                templateUrl: 'templates/modals/confirmation.html',
+                controller: 'ConfirmationModalCtrl',
+                size: 'sm'
+            });
+
+            return modalInstance;
         };
 
         $rootScope.menu_field_name.disable();
