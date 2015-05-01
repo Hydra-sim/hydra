5970311fce000d6623cbbc788b728029b07a61a8
SimenHjelden <hjesim12@student.westerdals.no>
2015-05-01 17:47:20 +0200

    Fixed autofocus on modals and so it works on each load of modal

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -31,4 +31,25 @@
        };
     });
 
+    //Directive for autofocus on modals every time it opens
+    // From: fakerun - http://stackoverflow.com/questions/28348342/how-to-set-focus-on-textarea-within-a-angular-ui-modal-every-time-open-the-modal
+    app.directive('focusMe', function($timeout) {
+        return {
+            scope: { trigger: '@focusMe' },
+            link: function(scope, element) {
+                scope.$watch('trigger', function(value) {
+                    if(value === "true") {
+                        $timeout(function() {
+                            element[0].focus();
+                        });
+                    } else{
+                        $timeout(function() {
+                            element[0].blur();
+                        });
+                    }
+                });
+            }
+        };
+    });
+
 })();
