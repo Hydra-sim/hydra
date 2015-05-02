1f47f6f8ed1a47d51fbcfce7d333444178b3e1f7
SimenHjelden <hjesim12@student.westerdals.no>
2015-05-02 16:02:26 +0200

    Implemented ngEnter directive and started working on implementing enter click on modals, having trouble with the validation

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -62,4 +62,23 @@
             }
         };
     });
+
+    app.directive('ngEnter', function($document) {
+        return {
+            scope: {
+                ngEnter: "&"
+            },
+            link: function(scope, element, attrs) {
+                var enterWatcher = function(event) {
+                    if (event.which === 13) {
+                        scope.ngEnter();
+                        scope.$apply();
+                        event.preventDefault();
+                        $document.unbind("keydown keypress", enterWatcher);
+                    }
+                };
+                $document.bind("keydown keypress", enterWatcher);
+            }
+        }
+    });
 })();
