3a800ef1b4f8de7cb889cb798682102dc037e6e8
Frank Lyder bredland <fl@nyggen.com>
2015-05-06 00:25:27 +0200

    Fixed code formating and removed unused pages in app.js

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -14,30 +14,45 @@
     ]);
 
     app.config(['uiZeroclipConfigProvider', function(uiZeroclipConfigProvider) {
-
         // config ZeroClipboard
         uiZeroclipConfigProvider.setZcConf({
             swfPath: 'vendor/zeroclipboard/dist/ZeroClipboard.swf'
         });
-
     }]);
 
-    app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {   // Turn off spinner in top left corner of
-        cfpLoadingBarProvider.includeSpinner = false;                       // loading bar
+    app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+        // Turn off spinner in top left corner of loading bar
+        cfpLoadingBarProvider.includeSpinner = false;
     }]);
 
     app.config(function ($routeProvider) {
         $routeProvider
-            .when('/',                  {controller: 'ApplicationCtrl',         templateUrl: 'templates/index.html'})
+            .when('/', {
+                controller: 'ApplicationCtrl',
+                templateUrl: 'templates/index.html'
+            })
 
             // Simulation
-            .when('/simulation/new',    {controller: 'SimulationNewCtrl',       templateUrl: 'templates/simulation/new.html'})
-            .when('/simulation/:id',    {controller: 'SimulationEditCtrl',      templateUrl: 'templates/simulation/new.html'})
-            .when('/result',            {controller: 'SimulationResultCtrl',    templateUrl: 'templates/simulation/result.html'})
-            .when('/show/:id/',         {controller: 'SimulationShowCtrl',      templateUrl: 'templates/simulation/show.html'})
-            .when('/simulation/:id' +
-                    '/auth',            {controller: 'AuthPathCtrl',            templateUrl: "templates/modals/passwordAuth.html"})
-
+            .when('/simulation/new', {
+                controller: 'SimulationNewCtrl',
+                templateUrl: 'templates/simulation/new.html'
+            })
+            .when('/simulation/:id', {
+                controller: 'SimulationEditCtrl',
+                templateUrl: 'templates/simulation/new.html'
+            })
+            .when('/result', {
+                controller: 'SimulationResultCtrl',
+                templateUrl: 'templates/simulation/result.html'
+            })
+            .when('/show/:id/', {
+                controller: 'SimulationShowCtrl',
+                templateUrl: 'templates/simulation/show.html'
+            })
+            .when('/simulation/:id/auth', {
+                controller: 'AuthPathCtrl',
+                templateUrl: "templates/modals/passwordAuth.html"
+            })
             .when('/simulation/:id/animation', {
                 controller: 'SimulationAnimationCtrl',
                 controllerAs: 'ctrl',
@@ -45,21 +60,32 @@
             })
 
             // Timetable
-            .when('/timetable',         {controller: 'TimetableCtrl',           templateUrl: 'templates/timetable/index.html'})
-            .when('/timetable/new',     {templateUrl: 'templates/timetable/new.html'})
-            .when('/timetable/:id/',    {controller: 'TimetableEditCtrl',       templateUrl: 'templates/timetable/show.html'})
+            .when('/timetable', {
+                controller: 'TimetableCtrl',
+                templateUrl: 'templates/timetable/index.html'
+            })
 
             // Preset
-            .when('/preset',            {controller: 'PresetCtrl',              templateUrl: 'templates/preset/index.html'})
-            .when('/preset/new',        {templateUrl: 'templates/preset/new.html'})
-            .when('/preset/:id/',       {templateUrl: 'templates/preset/show.html'})
+            .when('/preset', {
+                controller: 'PresetCtrl',
+                templateUrl: 'templates/preset/index.html'
+            })
+            .when('/preset/:id/', {
+                templateUrl: 'templates/preset/show.html'
+            })
 
-            .when('/map',               {controller: 'UploadMapCtrl',           templateUrl: "templates/map.html"})
-            .when('/documentation',     {controller: 'FullDocumentationCtrl',   templateUrl: 'templates/fulldocumentation.html' })
-            .when('/loadingscreen',     {templateUrl: 'templates/simulation/loadingscreen.html'})
+            // Other
+            .when('/map', {
+                controller: 'UploadMapCtrl',
+                templateUrl: "templates/map.html"
+            })
+            .when('/documentation', {
+                controller: 'FullDocumentationCtrl',
+                templateUrl: 'templates/fulldocumentation.html'
+            })
 
+            // Otherwise redirect to frontpage
             .otherwise({redirectTo : '/'})
     });
-
 
 })();
