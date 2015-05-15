f3d3be997fc4507a111675eb93ece6b7f851a366
Frank Lyder bredland <fl@nyggen.com>
2015-05-15 04:32:46 +0200

    Some more refactoring of the `MapModalCtrl` to make it independent

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -6,20 +6,11 @@
 
     app.controller('UploadMapCtrl', function($scope, $modal, Upload){
 
-        $scope.image = { visible: true, id: 86, exists: true, zoom: 0};
-        $scope.image2 = {scale: 1, zoom: 0};
-
-        // Create a variable to store the transform value
-        $scope.transform = "scale(" + $scope.image.zoom + ")";
+        $scope.image = {};
 
         // When the number changes, update the transform string
-        $scope.$watch("image.zoom", function() {
-            $scope.image.scale = ($scope.image.zoom / 50) + 1;
-            $scope.transform = "scale("+$scope.image.scale+")";
-        });
-
-        $scope.$watch("image2.scale", function() {
-            $scope.transform = "scale("+$scope.image2.scale+")";
+        $scope.$watch("image.zoom", function(value) {
+            $scope.image.scale = (value / 50) + 1;
         });
 
         $scope.openMapModal = function() {
@@ -29,28 +20,43 @@
                 file: $scope.file[0]
             }).success(function(data, status, headers, config) {
                 $scope.image.id = data.id;
+                $scope.image.zoom = data.zoom;
 
-                $modal.open({
+                var promise = $modal.open({
                     templateUrl: 'templates/modals/mapModal.html',
                     controller: 'MapModalCtrl',
                     controllerAs: 'ctrl',
                     resolve: {
-                        image: function () {
-                            return $scope.image;
+                        id: function () {
+                            return $scope.image.id;
                         }
                     }
+                });
+
+                promise.result.then(function(zoom) {
+                    $scope.image.zoom = zoom;
                 });
 
             });
         }
     });
 
-    app.controller('MapModalCtrl', function($modalInstance, image) {
-        this.image = image;
+    app.controller('MapModalCtrl', function($scope, $modalInstance, id) {
+        this.id = id;
+        this.zoom = 1;
+
         this.cancel = $modalInstance.dismiss;
         this.submitMap = function() {
-            $modalInstance.close(this.image);
+            $modalInstance.close(this.zoom);
         };
+
+        // When the number changes, update the transform string
+        var that = this;
+        $scope.$watch(
+            function() { return that.zoom; },
+            function(value) { that.scale = (value / 50) + 1;}
+        );
+
     });
 
 })();
