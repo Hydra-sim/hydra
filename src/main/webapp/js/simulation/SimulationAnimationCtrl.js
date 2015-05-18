8c74308f0e752563fc47648748b219f689330acd
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2015-05-18 03:49:10 +0200

    Animation now shows time. Bugs: Time is shown in AM/PM format, and it stops about 1 min 12 seconds short. Will be looking at this tomorrow, right now I need to sleep. Also commented out line 19 and 20 on SimulationAnimationCtrl, because it produces an error message that makes the page break. Made a bug for this in Jira.

--- src/main/webapp/js/simulation/SimulationAnimationCtrl.js
+++ src/main/webapp/js/simulation/SimulationAnimationCtrl.js
@@ -12,29 +12,61 @@
         $scope.image = {};
 
         Simulation.run({}, {id: $routeParams.id, breakpoints: 100}, function(result) {
+
             $scope.datasource.nodes = result.nodes;
             $scope.datasource.edges = result.relationships;
             $scope.totalSteps = result.tickBreakpoints -1;
-            $scope.image = result.map;
-            $scope.image.url = 'api/map/' + result.map.id;
+            //$scope.image = result.map;
+            //$scope.image.url = 'api/map/' + result.map.id;
+
+            var startTimeDate = new Date();
+            startTimeDate.setHours(0);
+            startTimeDate.setMinutes(0);
+            startTimeDate.setSeconds(result.startTick);
+            $scope.startTime = startTimeDate.toLocaleTimeString();
+
+            var endTimeDate = new Date();
+            endTimeDate.setHours(0);
+            endTimeDate.setMinutes(0);
+            endTimeDate.setSeconds(result.startTick + result.ticks);
+            $scope.endTime = endTimeDate.toLocaleTimeString();
+
+            $scope.currentTime.date = startTimeDate;
+
+            $scope.ticksBetweenSteps = result.ticks / result.tickBreakpoints;
+            var date = $scope.currentTime.date;
+            $scope.currentTime.seconds = date.getHours()*60*60 + date.getMinutes()*60 + date.getSeconds();
+
             update_datasource_progress();
         });
 
-        $scope.startTime = 1;
-        $scope.endTime = 8;
+        $scope.currentTime = {};
+        $scope.currentTime.date = new Date();
+
         $scope.steps = 1;
         $scope.progress = {};
         $scope.progress.position = 0;
+        $scope.currentTime = {};
         $scope.totalSteps = 7;
         $scope.control = {};
 
         function update_datasource_progress() {
-            _.each($scope.datasource.nodes, function(value, key, list) {
+            _.each($scope.datasource.nodes, function (value, key, list) {
                 $scope.datasource.nodes[key].progress = $scope.progress.position;
             });
+
+            if ($scope.currentTime.date) {
+
+                $scope.currentTime.date.setHours(0);
+                $scope.currentTime.date.setMinutes(0);
+
+                $scope.currentTime.date.setSeconds($scope.currentTime.seconds + ( $scope.ticksBetweenSteps * $scope.progress.position ));
+            }
+
         }
 
         $scope.$watchCollection('progress', function(newvalue) {
+
             update_datasource_progress();
             $scope.control.update();
         });
